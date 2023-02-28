a3f6aa3ae90af9b892b738441bb8ade4567e383e
Sk Sohail <sk.sohail2@wipro.com>
2023-02-28 21:06:23 +0530

    Basic TCP Communication

---
+++ client.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#define PORT 21
+int main(){
+int client_sock, bytes_sent, bytes_recv;
+struct sockaddr_in server_addr;
+char buffer[1024];// Create socket
+if ((client_sock = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+{
+perror("Socket creation failed");
+exit(1);
+}// Set server address
+server_addr.sin_family = AF_INET;
+server_addr.sin_port = htons(PORT);
+server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+bzero(&(server_addr.sin_zero), 8);// Connect to server
+if (connect(client_sock, (struct sockaddr *)&server_addr, sizeof(struct sockaddr)) == -1)
+{
+perror("Connect failed");
+exit(1);
+}// Receive welcome message from server
+bytes_recv = recv(client_sock, buffer, 1024, 0);
+if (bytes_recv == -1)
+{
+perror("Receive failed");
+exit(1);
+}
+buffer[bytes_recv] = '\0';
+printf("%s", buffer);// Close the socket
+close(client_sock);
+return 0;
+}
---
+++ server.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#define PORT 21
+
+int main()
+{
+int server_sock, client_sock, bytes_sent, bytes_recv;
+struct sockaddr_in server_addr, client_addr;
+char buffer[1024];// Create socket
+if ((server_sock = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+{
+perror("Socket creation failed");
+exit(1);
+}// Bind socket to port
+server_addr.sin_family = AF_INET;
+server_addr.sin_port = htons(PORT);
+server_addr.sin_addr.s_addr = INADDR_ANY;
+bzero(&(server_addr.sin_zero),8);
+if (bind(server_sock, (struct sockaddr *)&server_addr, sizeof(struct sockaddr)) == -1)
+{
+perror("Socket binding failed");
+exit(1);
+}// Listen for incoming connections
+if (listen(server_sock, 5) == -1)
+{
+perror("Listen failed");
+exit(1);
+}
+printf("FTP server waiting for connections...\n");// Accept incoming connections
+socklen_t client_len = sizeof(struct sockaddr_in);
+if ((client_sock = accept(server_sock, (struct sockaddr *)&client_addr, &client_len)) == -1)
+{
+perror("Accept failed");
+exit(1);
+}
+printf("Connection accepted from %s:%d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));// Send welcome message to client
+char welcome_msg[] = "220 Welcome to FTP server\r\n";
+bytes_sent = send(client_sock, welcome_msg, strlen(welcome_msg), 0);
+if (bytes_sent == -1)
+{
+perror("Send failed");
+exit(1);
+}// Close the sockets
+close(client_sock);
+close(server_sock);
+return 0;
+}
